f239b1ed184d914877b46fa702b98ddcc313851c
Meghthedev <meghthedev@gmail.com>
2023-05-15 10:09:39 +0530

    check: Send message via telegram instead
    * Google disabled the 'Allow less secure apps', henceforth it is much easier to rather use a telegram bot and channel

--- main.py
+++ main.py
@@ -13,6 +13,24 @@ if not os.path.exists(previous_data_file):
     with open(file_path, 'w') as f:
         f.write('Initial content')
 
+def notify():
+    # Replace YOUR_TOKEN with your actual bot token and YOUR_CHAT_ID with your actual chat ID
+    BOT_TOKEN = 'YOUR_TOKEN'
+    CHAT_ID = 'YOUR_CHAT_ID'
+
+    # Define the message you want to send
+    message = 'Site Updated (gseb.org)!'
+
+    # Send the message to the Telegram channel using the Telegram Bot API
+    url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}'
+    response = requests.get(url)
+
+    # Check if the message was sent successfully
+    if response.status_code == 200:
+        print('Message sent successfully')
+    else:
+        print(f'Error sending message: {response.content}')
+
 while True:
     # Retrieve the current website data
     response = requests.get(url, verify=False)
@@ -34,30 +52,3 @@ while True:
     # Wait for a certain amount of time before checking again
     time.sleep(300) # Check every 5 minutes
 
-def notify():
-    # Send a notification using your preferred method, such as email or push notification
-    sender_email = "your_email@gmail.com"
-    sender_password = "your_password"
-        
-    # The recipient's email address
-    recipient_email = "recipient_email@example.com"
-
-    # The subject and body of the email
-    subject = "Website update"
-    body = "The website has been updated!"
-
-    # Connect to the Gmail SMTP server
-    smtp_server = "smtp.gmail.com"
-    smtp_port = 587
-    smtp_connection = smtplib.SMTP(smtp_server, smtp_port)
-    smtp_connection.starttls()
-    smtp_connection.login(sender_email, sender_password)
-
-    # Create the email message
-    message = f"Subject: {subject}\n\n{body}"
-
-    # Send the email
-    smtp_connection.sendmail(sender_email, recipient_email, message)
-
-    # Close the SMTP connection
-    smtp_connection.quit()
